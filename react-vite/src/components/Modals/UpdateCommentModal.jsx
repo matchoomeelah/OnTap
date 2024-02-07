3e23da7429b8ca059303fb9c52c2feb7f294af27
Matt Miller <matchoomeelah@gmail.com>
2024-02-07 09:37:21 -0800

    Update Comment Form errors

--- react-vite/src/components/Modals/UpdateCommentModal.jsx
+++ react-vite/src/components/Modals/UpdateCommentModal.jsx
@@ -7,7 +7,7 @@ function UpdateCommentModal({beerId, comment}) {
     const dispatch = useDispatch();
     const { closeModal } = useModal();
 
-    const sessionUser = useSelector(state => state.session.user);
+    // const sessionUser = useSelector(state => state.session.user);
 
     const [body, setBody] = useState(comment.body);
     const [bodyCharCount, setBodyCharCount] = useState(body.length);
@@ -18,6 +18,11 @@ function UpdateCommentModal({beerId, comment}) {
         e.preventDefault();
         setErrors({});
 
+        if (body.trim().length <= 0) {
+            setErrors({ body: "Your comment cannot be blank" })
+            return
+        }
+
         const formData = new FormData();
         formData.append("body", body);
 
@@ -25,7 +30,6 @@ function UpdateCommentModal({beerId, comment}) {
 
         if (newComment.errors) {
             setErrors(newComment.errors);
-            setImageLoading(false);
         }
         else {
             closeModal();
@@ -42,11 +46,24 @@ function UpdateCommentModal({beerId, comment}) {
                     value={body}
                     onChange={e => {
                         setBody(e.target.value);
-                        setBodyCharCount(e.target.value.length)
+                        setBodyCharCount(e.target.value.length);
+                        if (errors.body) {
+                            const newErrors = {...errors};
+                            delete newErrors.body;
+                            setErrors(newErrors)
+                        }
                     }}
                     maxLength={255}
                 />
-                <div id="body-char-count">{bodyCharCount}/255</div>
+                <div id="error-count-div">
+                    <div className="error-container">
+                    {errors.body && <span className="error-message">*{errors.body}</span>}
+                    </div>
+                    <div id="body-char-count">{bodyCharCount}
+                        /255
+                    </div>
+                </div>
+                {/* <div id="body-char-count">{bodyCharCount}/255</div> */}
                 <button className='submit-button'>Confirm</button>
             </form>
         </div>
