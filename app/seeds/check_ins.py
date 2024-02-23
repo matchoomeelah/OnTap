4fb4f96ed6f2454b621175849708d6c6873fd579
Matt Miller <matchoomeelah@gmail.com>
2024-02-22 20:49:36 -0800

    Seeds

--- app/seeds/check_ins.py
+++ app/seeds/check_ins.py
@@ -80,80 +80,80 @@ def seed_check_ins():
     ),
     ]
 
-    fill_psych = [
-    CheckIn(
-        body="Spluh buh dubba doo",
-        rating=4,
-        image_url="https://i.ibb.co/4MtwFpc/IMG-20190427-182516060-HDR.jpg",
-        beer_id=3,
-        brewery_id=3,
-        user_id=6
-    ),
-    CheckIn(
-        body="I may be a demo user, but this ain't no demo beer!",
-        rating=4,
-        image_url="https://i.ibb.co/ZdfxN0Z/psych3.jpg",
-        beer_id=3,
-        brewery_id=3,
-        user_id=5
-    ),
-    CheckIn(
-        body="I may be a demo user, but this ain't no demo beer!",
-        rating=4,
-        image_url="https://i.ibb.co/jW025Dx/ex-beer-2.jpg",
-        beer_id=3,
-        brewery_id=3,
-        user_id=4
-    ),
-    CheckIn(
-        body="I may be a demo user, but this ain't no demo beer!",
-        rating=4,
-        image_url="https://i.ibb.co/tpJKy09/image-99ea41ab-378a-4f93-955b-daa6d6ae97a4-600x.webp",
-        beer_id=3,
-        brewery_id=3,
-        user_id=3
-    ),
-    CheckIn(
-        body="I may be a demo user, but this ain't no demo beer!",
-        rating=4,
-        image_url="https://i.ibb.co/yqDfQJB/madtree-psychopathy-og.webp",
-        beer_id=3,
-        brewery_id=3,
-        user_id=2
-    ),
-    CheckIn(
-        body="I may be a demo user, but this ain't no demo beer!",
-        rating=4,
-        image_url="https://i.ibb.co/5G0WpKj/psychopathy1.jpg",
-        beer_id=3,
-        brewery_id=3,
-        user_id=1
-    ),
-    CheckIn(
-        body="I may be a demo user, but this ain't no demo beer!",
-        rating=4,
-        image_url="https://i.ibb.co/fNj843Y/0085833600425-A.webp",
-        beer_id=3,
-        brewery_id=3,
-        user_id=3
-    ),
-    CheckIn(
-        body="It's just like the story of the grasshopper and the octopus. All year long, the grasshopper kept burying acorns for the winter, while the octopus mooched off his girlfriend and watched TV. But then the winter came, and the grasshopper died, and the octopus ate all his acorns.",
-        rating=5,
-        image_url="https://i.ibb.co/HtTwC82/DSC-0012-1024x683.jpg",
-        beer_id=3,
-        brewery_id=3,
-        user_id=2
-    ),
-    CheckIn(
-        body="More pleeeaaase.",
-        rating=5,
-        image_url="https://i.ibb.co/GxHsJMm/ex-beer-5.webp",
-        beer_id=3,
-        brewery_id=3,
-        user_id=1
-    ),
-    ]
+    # fill_psych = [
+    # CheckIn(
+    #     body="Spluh buh dubba doo",
+    #     rating=4,
+    #     image_url="https://i.ibb.co/4MtwFpc/IMG-20190427-182516060-HDR.jpg",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=6
+    # ),
+    # CheckIn(
+    #     body="I may be a demo user, but this ain't no demo beer!",
+    #     rating=4,
+    #     image_url="https://i.ibb.co/ZdfxN0Z/psych3.jpg",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=5
+    # ),
+    # CheckIn(
+    #     body="I may be a demo user, but this ain't no demo beer!",
+    #     rating=4,
+    #     image_url="https://i.ibb.co/jW025Dx/ex-beer-2.jpg",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=4
+    # ),
+    # CheckIn(
+    #     body="I may be a demo user, but this ain't no demo beer!",
+    #     rating=4,
+    #     image_url="https://i.ibb.co/tpJKy09/image-99ea41ab-378a-4f93-955b-daa6d6ae97a4-600x.webp",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=3
+    # ),
+    # CheckIn(
+    #     body="I may be a demo user, but this ain't no demo beer!",
+    #     rating=4,
+    #     image_url="https://i.ibb.co/yqDfQJB/madtree-psychopathy-og.webp",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=2
+    # ),
+    # CheckIn(
+    #     body="I may be a demo user, but this ain't no demo beer!",
+    #     rating=4,
+    #     image_url="https://i.ibb.co/5G0WpKj/psychopathy1.jpg",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=1
+    # ),
+    # CheckIn(
+    #     body="I may be a demo user, but this ain't no demo beer!",
+    #     rating=4,
+    #     image_url="https://i.ibb.co/fNj843Y/0085833600425-A.webp",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=3
+    # ),
+    # CheckIn(
+    #     body="It's just like the story of the grasshopper and the octopus. All year long, the grasshopper kept burying acorns for the winter, while the octopus mooched off his girlfriend and watched TV. But then the winter came, and the grasshopper died, and the octopus ate all his acorns.",
+    #     rating=5,
+    #     image_url="https://i.ibb.co/HtTwC82/DSC-0012-1024x683.jpg",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=2
+    # ),
+    # CheckIn(
+    #     body="More pleeeaaase.",
+    #     rating=5,
+    #     image_url="https://i.ibb.co/GxHsJMm/ex-beer-5.webp",
+    #     beer_id=3,
+    #     brewery_id=3,
+    #     user_id=1
+    # ),
+    # ]
 
 
 
